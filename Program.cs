260818fafd768218819b416e6407349b93df47e0
kaci <kaci.school.info@gmail.com>
2023-02-24 14:06:50 -0600

    phase1

--- Program.cs
+++ Program.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 
-namespace GuessingGame
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+// Display a message to the user asking them to guess the secret number.
+Console.WriteLine("Time to guess a secret number!");
+// Display a prompt for the user's guess.
+string input = Console.ReadLine();
+int parsedInput = int.Parse(input);
+// Take the user's guess as input and save it as a variable.
+// Display the user's guess back to the screen.
+Console.WriteLine("You guessed:");
+Console.WriteLine(parsedInput);
