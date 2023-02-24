71f33b3aa8601e9d60f02672447bcabd3657aba6
kaci <kaci.school.info@gmail.com>
2023-02-24 14:11:36 -0600

    phase2

--- Program.cs
+++ Program.cs
@@ -7,5 +7,17 @@ string input = Console.ReadLine();
 int parsedInput = int.Parse(input);
 // Take the user's guess as input and save it as a variable.
 // Display the user's guess back to the screen.
-Console.WriteLine("You guessed:");
-Console.WriteLine(parsedInput);
+
+// Create a variable to contain the secret number. This number should be hard-coded for now. 42 is a nice number.
+int secretNum = 42;
+// No longer display the user's guess. They know what they guessed, right?
+// Compare the user's guess with the secret number. Display a success message if the guess is correct, 
+// otherwise display a failure message.
+if (parsedInput == secretNum)
+{
+    Console.WriteLine("Correct!");
+}
+else
+{
+    Console.WriteLine("FAIL");
+}
