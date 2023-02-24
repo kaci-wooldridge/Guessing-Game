5d56ff340fd38978949eb9dc743b534093f99c06
kaci <kaci.school.info@gmail.com>
2023-02-24 14:15:06 -0600

    phase3

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 
 // Display a message to the user asking them to guess the secret number.
-Console.WriteLine("Time to guess a secret number!");
+// Console.WriteLine("Time to guess a secret number!");
 // Display a prompt for the user's guess.
-string input = Console.ReadLine();
-int parsedInput = int.Parse(input);
+// string input = Console.ReadLine();
+// int parsedInput = int.Parse(input);
 // Take the user's guess as input and save it as a variable.
 // Display the user's guess back to the screen.
 
@@ -13,11 +13,30 @@ int secretNum = 42;
 // No longer display the user's guess. They know what they guessed, right?
 // Compare the user's guess with the secret number. Display a success message if the guess is correct, 
 // otherwise display a failure message.
-if (parsedInput == secretNum)
+// if (parsedInput == secretNum)
+// {
+//     Console.WriteLine("Correct!");
+// }
+// else
+// {
+//     Console.WriteLine("FAIL");
+// }
+
+// Give the user four chances to guess the number.
+for (int i = 0; i < 4; i++)
 {
-    Console.WriteLine("Correct!");
+    Console.WriteLine("Time to guess a secret number!");
+    string input = Console.ReadLine();
+    int parsedInput = int.Parse(input);
+
+    if (parsedInput == secretNum)
+    {
+        Console.WriteLine("Correct!");
+    }
+    else
+    {
+        Console.WriteLine("FAIL");
+    }
 }
-else
-{
-    Console.WriteLine("FAIL");
-}
+
+// Continue to display the success or failure messages as in phase 2
