737b599884bb2a665c388a9abd54314c9afb5c0a
kaci <kaci.school.info@gmail.com>
2023-02-27 10:12:02 -0600

    finished project

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@
 // Display the user's guess back to the screen.
 
 // Create a variable to contain the secret number. This number should be hard-coded for now. 42 is a nice number.
-int secretNum = 42;
+// int secretNum = 42;
 // No longer display the user's guess. They know what they guessed, right?
 // Compare the user's guess with the secret number. Display a success message if the guess is correct, 
 // otherwise display a failure message.
@@ -30,21 +30,68 @@ int secretNum = 42;
 
 // End the loop early if the user guesses the correct number.
 
-for (int i = 0; i < 4; i++)
+// Use a random number to set the secret number between 1 and 100 instead of a hard-coded number.
+// The prompt should display the number of guesses the user has left.
+Random rnd = new Random();
+int secretNum = rnd.Next(0, 100);
+
+int diffFun (string choice)
+{
+    int x = 0;
+    if(choice == "easy")
+    {
+        x = 8;
+    }
+    else if(choice == "medium")
+    {
+        x = 6;
+    }
+    else if(choice == "hard")
+    {
+        x = 4;
+    }
+    else if(choice == "cheater")
+    {
+        x = 100;
+    }
+    else
+    {
+    }
+    return x;
+}
+
+Console.WriteLine("Welcome to Guessing Game");
+Console.WriteLine("Choose a difficulty");
+Console.WriteLine("Easy, Medium, Hard, or Cheater?");
+string diffChoice = Console.ReadLine().ToLower();
+int x = diffFun(diffChoice);
+
+for (int i = 0; i < x; i++)
 {
     Console.WriteLine("Time to guess a secret number!");
     string input = Console.ReadLine();
     int parsedInput = int.Parse(input);
 
-    if (parsedInput == secretNum)
+    if (parsedInput > secretNum)
     {
         Console.WriteLine("Your guess: " + parsedInput);
-        Console.WriteLine("Correct!");
-        i += 4;
+        Console.WriteLine("Too high!");
+        int remaining = x - i - 1;
+        Console.WriteLine("guesses remaining: " + remaining);
+        Console.WriteLine("_ _ _ _ _ _ _ _ _ _");
+    }
+    else if (parsedInput < secretNum)
+    {
+        Console.WriteLine("Your guess: " + parsedInput);
+        Console.WriteLine("Too low!");
+        int remaining = x - i - 1;
+        Console.WriteLine("guesses remaining: " + remaining);
+        Console.WriteLine("_ _ _ _ _ _ _ _ _ _");
     }
     else
     {
         Console.WriteLine("Your guess: " + parsedInput);
-        Console.WriteLine("FAIL");
+        Console.WriteLine("Lucky guess!");
+        break;
     }
 }
